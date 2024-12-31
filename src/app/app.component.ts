f76db784ae45e18dcb67938f9ec962a138ba4c58
cisse410 <dramisse90@gmail.com>
2024-12-31 22:51:57 +0000

    communication entre le composant et le template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,8 +7,24 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-team';
+
+  team = {
+    id: 'fc-barcelona',
+    name: 'FC Barcelone',
+    country: '🇪🇸',
+    foundedAt: '1899-11-29',
+    coach: 'Xavi Hernandez',
+    budget: 606000000,
+    imageURL: 'assets/fc-barcelona.jpg',
+    twitter: 'FCBarcelona',
+    champions: [1992, 2006, 2009, 2011, 2015],
+  };
+
+  onClick(link: HTMLAnchorElement) {
+    console.log('onClick', link);
+  }
 }
