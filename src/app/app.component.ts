6ccaa8a796add59cbee4bf6104019a45ff88cd87
cisse410 <dramisse90@gmail.com>
2025-01-01 23:35:45 +0000

    Ajout de '@vercel/analytics' pour les statisqtiques du site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
