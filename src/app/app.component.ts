9ca1dcc0ad2d64957a85b0d4a56754e796ae0ba0
cisse410 <dramisse90@gmail.com>
2025-01-01 23:39:14 +0000

    complete the enabling vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { inject } from '@vercel/analytics';
+inject();
 
 @Component({
   selector: 'app-root',
